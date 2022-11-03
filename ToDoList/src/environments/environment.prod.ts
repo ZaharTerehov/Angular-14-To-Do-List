d1853875d102884f969d43e2519bb6b957af3a13
z.terehov <zaharkuli67@gmail.com>
2022-11-03 20:17:21 +0300

    connected firebase

--- ToDoList/src/environments/environment.prod.ts
+++ ToDoList/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:
+    {
+      apiKey: "AIzaSyBGNVEqaF2AzCG0mPSZDPxC5wp9c9pCWNk",
+      authDomain: "todolistapp-fad35.firebaseapp.com",
+      projectId: "todolistapp-fad35",
+      storageBucket: "todolistapp-fad35.appspot.com",
+      messagingSenderId: "324352436271",
+      appId: "1:324352436271:web:b648fd8efea4fdda9bfeda"
+    }
 };
--- ToDoList/src/environments/environment.ts
+++ ToDoList/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:
+    {
+      apiKey: "AIzaSyBGNVEqaF2AzCG0mPSZDPxC5wp9c9pCWNk",
+      authDomain: "todolistapp-fad35.firebaseapp.com",
+      projectId: "todolistapp-fad35",
+      storageBucket: "todolistapp-fad35.appspot.com",
+      messagingSenderId: "324352436271",
+      appId: "1:324352436271:web:b648fd8efea4fdda9bfeda"
+    }
 };
 
 /*
